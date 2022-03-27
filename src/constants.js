93d62cc235fccce296501c283fbe456d9435b1a0
hakngrow <hakngrow@gmail.com>
2022-03-27 13:13:01 +0800

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x232AF238cfb0bAE4F727cDE4C7D42a77E973d7BF";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
